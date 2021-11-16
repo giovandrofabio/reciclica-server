f853ce3f0cd848c92ef02cf61fe6cea7cfcc8f7a
Paulo Alves <paulorsaf@gmail.com>
2021-11-16 17:44:36 +0100

    Creating register module

---
+++ src/modules/register/register.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post } from '@nestjs/common';
+import { RegisterService } from './register.service';
+
+@Controller('register')
+export class RegisterController {
+  constructor(private readonly registerService: RegisterService) {}
+
+  @Post()
+  register() {
+    return "user registered";
+  }
+}
